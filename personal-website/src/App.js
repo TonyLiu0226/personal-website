dd3ff37879692c6771aa8921cafe2ccc9b9399a9
Tony Liu <tl0226yn@gmail.com>
2022-05-02 22:46:25 -0700

    installs mui

--- personal-website/src/App.js
+++ personal-website/src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import React, {useState, useEffect } from 'react'
 
 function App() {
+
+  const [data, setData] = useState([{}])
   return (
     <div className="App">
       <p>Bruh</p>
